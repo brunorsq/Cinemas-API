c5360d8f82c1fc50cba75d2772c0d45a10f61076
Bruno Rousenq <bruno.rsq@hotmail.com>
2023-04-30 18:06:16 -0300

    Criada a entidade sessao para relacionar Filmes e Cinemas

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -19,6 +19,8 @@ namespace FilmesAPI.Models
 
         [Range(1, 600, ErrorMessage = "A duração deve ser entre 1 e 600 minutos")]
         public int Duracao { get; set; }
+
+        public virtual List<Sessao> Sessoes { get; set; }
         
     }
 }
